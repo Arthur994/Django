0f6d54e412dc195acf6d4fb75dfc838f50187308
arthur <arthurr.sa@hotmail.com>
2019-08-28 14:19:40 -0300

    alterações visuais e paginação

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post_list.html',{'posts':posts})
+    post_list = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(post_list, 3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 def about(request):
     return render(request, 'blog/about.html')
